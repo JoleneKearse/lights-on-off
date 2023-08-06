c1c91788ec82836cc938fa726573af13d30d2f2d
Jolene Kearse <pharaohnutz@gmail.com>
2023-08-06 10:10:20 -0400

    feat: add boxes to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import boxData from "./boxes"
+import { useState } from "react"
+import "./index.css"
 
 function App() {
+  const [boxArr, setBoxArr] = useState(boxData)
+  const boxEl = boxArr.map(box => {
+    return (
+      <div className="box" key={box.id}></div>
+    )
+  })
 
   return (
     <>
-
+      <main>
+        {boxEl}
+      </main>
     </>
   )
 }
